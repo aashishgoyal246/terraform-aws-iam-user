51fa9c8daa87cf0cd37c150f5d291f342435c293
Aashish Goyal <aashishgoyal.246@gmail.com>
2020-06-29 17:11:03 +0530

    Updated outputs.tf

--- _example/outputs.tf
+++ _example/outputs.tf
@@ -1,22 +1,22 @@
 # Module      : IAM User
 # Description : Terraform module to create Iam User resource on AWS.
 output "arn" {
-  value       = module.iam_user.*.arn
+  value       = module.iam_user.arn
   description = "The ARN assigned by AWS for this user."
 }
 
 output "unique_id" {
-  value       = module.iam_user.*.unique_id
+  value       = module.iam_user.unique_id
   description = "The unique ID assigned by AWS for this user."
 }
 
 output "key_id" {
-  value       = module.iam_user.*.key_id
+  value       = module.iam_user.key_id
   description = "The ARN assigned by AWS for this user."
 }
 
 output "secret" {
-  value       = module.iam_user.*.secret
+  value       = module.iam_user.secret
   description = "The ARN assigned by AWS for this user."
 }
 
